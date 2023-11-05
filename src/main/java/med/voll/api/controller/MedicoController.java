659f1162b8a581b6e6081d080cc8d718e3e17f3c
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2023-11-04 21:44:49 -0300

    CRUD completo para medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,14 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,8 +19,30 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar (@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+    return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @GetMapping("/all-infos")
+    public Page<Medico> listarTodasInfos(Pageable paginacao) {
+        return repository.findAll(paginacao);
+    }
+
+    @PutMapping
+    @Transactional
+    public void editar (@RequestBody @Valid DadosEditarMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
